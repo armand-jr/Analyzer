a5b9a74b15e8d8470a8b8a94a552dbf108ae1ee0
Armand <36383558+armand-jr@users.noreply.github.com>
2024-04-23 21:46:57 +0200

    Add files via upload

--- find.py
+++ find.py
@@ -1,9 +1,10 @@
 import sys
 
-# Ensure that the script is run with a file argument
 if len(sys.argv) < 2:
-    print("Usage: python script.py <filename>")
+    print("Usage: python3 find.py <filename>")
     sys.exit(1)
+
+incassos = ['casso']
 
 filename = sys.argv[1]
 
@@ -15,21 +16,21 @@ try:
                 if 'C' in line:
                     parts = line.split('C')
                     transaction_type = '+'
-                elif 'D' in line:
+                if 'D' in line:
                     parts = line.split('D')
                     transaction_type = '-'
                 
                 if len(parts) > 1:
                     # Extract the amount
                     amount_part = parts[1]
-                    amount = amount_part.split('N')[0]
+                    amount = amount_part.split('N')[0].strip()
                     print(f"{transaction_type}{amount.strip()}")
                     
             
             
             if line.startswith(':86:'):
                 #Extract and store transaction description
-                name = line[4:].strip()
+                name = line[5:].strip()
                 print(name)
             
 
@@ -37,46 +38,3 @@ except FileNotFoundError:
     print(f"Error: File not found '{filename}'")
 except Exception as e:
     print(f"An error occurred: {e}")
-
-
-
-
-
-
-
-""" 
-def parse_mt940(filename):
-    transactions = []  # to store transaction details
-
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-
-    current_transaction = {}
-    for line in lines:
-        line = line.strip()  # Remove any leading/trailing whitespace
-
-        if line.startswith(':86:'):
-            # Extract everything after ':86:'
-            description = line[4:].strip()
-            current_transaction['Description'] = description
-
-        if line.startswith(':62F:'):
-            # Extract the amount after 'EUR'
-            amount_pos = line.find('EUR')
-            if amount_pos != -1:
-                # Get the amount by finding the next space after 'EUR'
-                amount = line[amount_pos+3:].strip()
-                current_transaction['Amount'] = amount
-                # Save the transaction and prepare for the next
-                transactions.append(current_transaction)
-                current_transaction = {}  # Reset for next transaction
-
-    return transactions
-
-# Example usage
-
-transactions = parse_mt940(filename)
-for transaction in transactions:
-    print(transaction)
-
-"""

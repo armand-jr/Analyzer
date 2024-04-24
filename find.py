19a6328b296039b418af552126b0aac033a895d2
Armand <36383558+armand-jr@users.noreply.github.com>
2024-04-24 22:01:14 +0200

    Add files via upload

--- find.py
+++ find.py
@@ -4,14 +4,19 @@ if len(sys.argv) < 2:
     print("Usage: python3 find.py <filename>")
     sys.exit(1)
 
-incassos = ['casso']
+incassos = ['cjib ', 'Flanderijn ', 'Bierens ', 'Avi ', 'Syncasso', 'straetus', 'GGN ', 'Vesting', 'Debtt', 'Gerechtsdeurwaarder', 'korenhof', 'KBKP', 'Collect4u', 'Actis', 'derdenbeslag', 'invorderings', ' BRU ', 'Hoist', ' BVCM', 'coeo incasso', 'intrum', 'alektum']
+loterijen = ['Toto', 'casino', 'loterij', 'unibet', 'Bitvavo', 'Crypto', ' bet ', 'poker', 'coinbase']
 
 filename = sys.argv[1]
+flag = False
+
 
 try:
-    with open(filename, 'r') as file:
+   with open(filename, 'r') as file:
+        last_line = ''
+        flag = False
         for line in file:
-            line = line.strip()  # Remove whitespace
+            line = line.strip()  
             if line.startswith(':61:'):
                 if 'C' in line:
                     parts = line.split('C')
@@ -21,17 +26,26 @@ try:
                     transaction_type = '-'
                 
                 if len(parts) > 1:
-                    # Extract the amount
                     amount_part = parts[1]
                     amount = amount_part.split('N')[0].strip()
-                    print(f"{transaction_type}{amount.strip()}")
-                    
+                last_line = f"{transaction_type}{amount.strip()}"
+                flag = False
             
+            if line.lower().startswith(':86:'):
+                if any(word.lower() in line.lower() for word in incassos):
+                    flag = True
+                    name = line[5:].strip()
             
-            if line.startswith(':86:'):
-                #Extract and store transaction description
-                name = line[5:].strip()
-                print(name)
+            if line.lower().startswith(':86:'):
+                if any(word.lower() in line.lower() for word in loterijen):
+                    flag = True
+                    name = line[5:].strip()
+            
+            if flag:
+                print(last_line)
+                print(f"{name}")
+                flag = False
+
             
 
 except FileNotFoundError:
